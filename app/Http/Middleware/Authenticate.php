5b20957437887908caccdc949e929bfe46a42d01
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-22 22:49:22 +0600

    Added AUthenticate file for Laravel 8.x

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        if (!$request->expectsJson()) {
+            if (Auth::guard('admin')) {
+                return redirect()->route('admin.login');
+            } else {
+                return redirect()->route('login');
+            }
+        }
+    }
+}
