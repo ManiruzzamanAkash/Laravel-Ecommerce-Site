33767c3e0b11cce1367dadcec8699f203940c871
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-22 22:48:35 +0600

    Fixed RedirectIfAuthenticated file compatible issue

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,35 +2,31 @@
 
 namespace App\Http\Middleware;
 
+use App\Providers\RouteServiceProvider;
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
-  /**
-  * Handle an incoming request.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @param  \Closure  $next
-  * @param  string|null  $guard
-  * @return mixed
-  */
-  public function handle($request, Closure $next, $guard = null)
-  {
-    switch ($guard) {
-      case 'admin':
-      if (Auth::guard($guard)->check()) {
-        return redirect()->route('admin.index');
-      }
-      break;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  ...$guards
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, ...$guards)
+    {
+        $guards = empty($guards) ? [null] : $guards;
 
-      case 'web':
-      if (Auth::guard($guard)->check()) {
-        return redirect()->route('user.dashboard');
-      }
-      break;
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                return redirect(RouteServiceProvider::HOME);
+            }
+        }
+
+        return $next($request);
     }
-
-    return $next($request);
-  }
 }
